dad606e84d7dd43a1aa7a3b6d5a492c7b1864410
Vious <viousxie@outlook.com>
2024-12-10 22:29:01 +0800

    slidewin/prefix sum

--- ForFun/slidingWindow_prefixsum.cpp
+++ ForFun/slidingWindow_prefixsum.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <ranges>
 #include <queue>
+#include <numeric>
 
 
 using namespace std;
@@ -192,6 +193,94 @@ long long countSubarrays(vector<int>& nums, int k) {
     return result;
 }
 
+/* 2302. Count Subarrays With Score Less Than K */
+long long countSubarrays2(vector<int>& nums, long long k) {
+    int size = nums.size();
+    if (size <= 1) {
+        return (size == 1) ? (nums[0] < k ? 1 : 0) : 0; 
+    }
+    int left = 0;
+    long long tmpSum = 0;
+    long long result = 0;
+    for (int i = 0; i < size; i++) {
+        tmpSum += nums[i];
+        while(tmpSum * (i - left + 1) >= k) {
+            tmpSum -= nums[left];
+            left++;
+        }
+        result += (i - left + 1); 
+    }
+    return result;
+}
+
+/* 1658. Minimum Operations to Reduce X to Zero */
+int minOperations(vector<int>& nums, int x) {
+    int size = nums.size();
+    if (size == 1) {
+        return x == nums[0] ? 1 : -1;
+    }
+    int accum = std::accumulate(nums.begin(), nums.end(), 0);
+    if (accum < x) {
+        return -1;
+    }
+    // do the reverse work, find max subarray that match accum - x;
+    int left = 0, target = accum - x;
+    int result = -1, tmpSum = 0;
+    for (int i = 0; i < size; i++) {
+        tmpSum += nums[i];
+        while(tmpSum > target) {
+            tmpSum -= nums[left];
+            left++;
+        }
+        if (tmpSum == target) {
+            result = std::max(result, i - left + 1);
+        }
+    }
+    return result == -1 ? -1 : size - result; 
+}
+
+/* 76. Minimum Window Substring */
+bool checkCovering(int cntS[], int cntT[]) {
+    for (int i = 'a'; i <= 'z'; i++) {
+        if (cntS[i] < cntT[i]) {
+            return false;
+        }
+    }
+
+    for (int i = 'A'; i <= 'Z'; i++) {
+        if (cntS[i] < cntT[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+string minWindow(string s, string t) {
+    std::string result;
+    int size = s.size();
+    if (size < t.size()) {
+        return result;
+    }
+    int left = 0;
+    int indx = -1, minLen = INT32_MAX;
+    int cntS[128]{}, cntT[128]{};
+    for(char ch : t) {
+        cntT[ch]++;
+    }
+    for (int i = 0; i < size; i++) {
+        cntS[s[i]]++;
+        while(checkCovering(cntS, cntT)) {
+            if (minLen > (i - left + 1)) {
+                minLen = (i - left + 1);
+                indx = left;
+            }
+            cntS[s[left]]--;
+            left++;
+        }
+    }
+    return indx == -1 ? "" : s.substr(indx, minLen);
+}
+
 int main()
 {
     std::vector<int> nums = {10,5,2,6};
@@ -212,6 +301,11 @@ int main()
     // std::vector<int> nums3 = {100000};
     // std::cout << "2779. Maximum Beauty of an Array After Applying Operation : nums3 = {100000} " << maximumBeauty(nums3, 0) << std::endl;
 
+    std::vector<int> nums3 = {2,1,4,3,5};
+    std::cout << "2302. Count Subarrays With Score Less Than K : nums = [2,1,4,3,5], k = 10, Output: " << countSubarrays2(nums3, 10) << std::endl;
+
+    // std::vector<int> nums4 = {1,1,4,2,3};
+    // std::cout << "1658. Minimum Operations to Reduce X to Zero : nums = [1,1,4,2,3], x = 5, Output: " << minOperations(nums3, 5) << std::endl;
 
 
     return 0;
